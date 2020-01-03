b70aa2602a373081edf5464338cce6be576f015d
Bjørn Tore Håvie <bjoern.tore.haavie@pexip.com>
2020-01-03 11:31:27 +0100

    clean up preview, don't use full path in fzf

--- zsh-fzf-ghq.plugin.zsh
+++ zsh-fzf-ghq.plugin.zsh
@@ -1,8 +1,10 @@
 function ghq-fzf() {
-  local target_dir=$(ghq list -p | \
+  local target_dir=$(ghq list | \
 	  fzf --query="$LBUFFER" \
-	  --preview='(stat {}/README.md 2>&1 > /dev/null && highlight -O ansi {}/README.md) || ls --color=always {}'
+	  --preview='f() { ([ -f "$1/README.md" ] && highlight -O ansi "$1/README.md") || ls --color=always "$1"; }; \
+	    f $(ghq list --full-path --exact "{}")'
   )
+  target_dir=$(ghq list --exact --full-path "$target_dir")
 
   if [ -n "$target_dir" ]; then
     BUFFER="cd ${target_dir}"
